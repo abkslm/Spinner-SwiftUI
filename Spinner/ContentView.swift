b0782ce8e91a5c5db2317e63fedd5159362427cb
A. Blakeslee Moore <andrew@abkslm.com>
2023-10-26 12:07:34 -0700

    Initial Commit

---
+++ Spinner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spinner
+//
+//  Created by A. Blakeslee Moore on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spinner/SpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinnerApp.swift
+//  Spinner
+//
+//  Created by A. Blakeslee Moore on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
